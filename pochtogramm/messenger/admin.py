cb9741fb60f5d28b5980642bd29809eee5ddf19e
grand-roman@yandex.ru <grand-roman@yandex.ru>
2021-02-08 17:15:40 +0700

    add admin

--- pochtogramm/messenger/admin.py
+++ pochtogramm/messenger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'date', 'author')
+    search_fields = ('text',)
+    list_filter = ('date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Message, MessageAdmin)
